cf20d4f4c2e7e6e3f83a80915fd2831a43d463a7
Andr3iix <mgxandr3ix@gmail.com>
2024-11-24 17:50:25 -0500

    5:50 p.m. domingo, 24 de noviembre de 2024 (GMT-5) Hora en Urb San Ignacio

---
+++ script.js
@@ -0,0 +1,10 @@
+// script.js
+document.addEventListener('DOMContentLoaded', () => {
+    const hamburger = document.getElementById('hamburger');
+    const navMenu = document.getElementById('nav-menu');
+  
+    hamburger.addEventListener('click', () => {
+      navMenu.querySelector('ul').classList.toggle('active');
+    });
+  });
+  
